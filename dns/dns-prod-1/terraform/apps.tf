b07e4269ba35d7322f25fea8255e6a2e63fe49ff
kinzoku-dev <kinzoku@the-nebula.xyz>
2024-06-20 14:04:07 -0500

    feat(dns): add some dns records

--- dns/dns-prod-1/terraform/apps.tf
+++ dns/dns-prod-1/terraform/apps.tf
@@ -11,51 +11,9 @@ resource "dns_a_record_set" "minio-prod-1_s3" {
   ttl       = 3600
 }
 
-resource "dns_a_record_set" "portainer-prod-1" {
+resource "dns_a_record_set" "mdb_prod_1" {
   zone      = "home.kinzoku.dev."
-  name      = "portainer-prod-1.srv-prod-1"
-  addresses = ["192.168.3.15"]
-  ttl       = 3600
-}
-
-resource "dns_a_record_set" "prometheus-prod-1" {
-  zone      = "home.kinzoku.dev."
-  name      = "prometheus-prod-1.srv-prod-1"
-  addresses = ["192.168.3.15"]
-  ttl       = 3600
-}
-
-resource "dns_a_record_set" "grafana-prod-1" {
-  zone      = "home.kinzoku.dev."
-  name      = "grafana-prod-1.srv-prod-1"
-  addresses = ["192.168.3.15"]
-  ttl       = 3600
-}
-
-resource "dns_a_record_set" "cadvisor-prod-1" {
-  zone      = "home.kinzoku.dev."
-  name      = "cadvisor-prod-1.srv-prod-1"
-  addresses = ["192.168.3.15"]
-  ttl       = 3600
-}
-
-resource "dns_a_record_set" "cadvisor-prod-2" {
-  zone      = "home.kinzoku.dev."
-  name      = "cadvisor-prod-2.srv-prod-2"
-  addresses = ["192.168.3.16"]
-  ttl       = 3600
-}
-
-resource "dns_a_record_set" "loki-prod-1" {
-  zone      = "home.kinzoku.dev."
-  name      = "loki-prod-1.srv-prod-1"
-  addresses = ["192.168.3.15"]
-  ttl       = 3600
-}
-
-resource "dns_a_record_set" "ixdb-prod-1" {
-  zone      = "home.kinzoku.dev."
-  name      = "ixdb-prod-1.srv-prod-2"
+  name      = "mdb-prod-1"
   addresses = ["192.168.3.16"]
   ttl       = 3600
 }
--- dns/dns-prod-1/terraform/server.tf
+++ dns/dns-prod-1/terraform/server.tf
@@ -5,9 +5,23 @@ resource "dns_a_record_set" "srv_prod_1" {
   ttl       = 3600
 }
 
+resource "dns_a_record_set" "srv_prod_1_wildcard" {
+  zone      = "home.kinzoku.dev."
+  name      = "*.srv-prod-1"
+  addresses = ["192.168.3.15"]
+  ttl       = 3600
+}
+
 resource "dns_a_record_set" "srv_prod_2" {
   zone      = "home.kinzoku.dev."
   name      = "srv-prod-2"
+  addresses = ["192.168.3.16"]
+  ttl       = 3600
+}
+
+resource "dns_a_record_set" "srv_prod_2_wildcard" {
+  zone      = "home.kinzoku.dev."
+  name      = "*.srv-prod-2"
   addresses = ["192.168.3.16"]
   ttl       = 3600
 }
